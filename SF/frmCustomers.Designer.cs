a5d1e22633498a34d99f052bbc84e642c538ab1e
Stephen Curry <FUR08026512@live.nwrc.ac.uk>
2020-03-04 15:31:28 +0000

    p

--- SF/frmCustomers.Designer.cs
+++ SF/frmCustomers.Designer.cs
@@ -503,7 +503,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(143, 0);
+            this.label1.Location = new System.Drawing.Point(122, 1);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(32, 13);
             this.label1.TabIndex = 18;

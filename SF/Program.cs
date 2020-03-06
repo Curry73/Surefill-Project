d7ce3b1ebea994019a37b29188dcaf33926f3334
Marty Sweeney <marty.sweeney@fujitsu.com>
2020-03-06 15:47:04 +0000

    MS PM

--- SF/Program.cs
+++ SF/Program.cs
@@ -16,7 +16,7 @@ namespace SF
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmCustomers());
+            Application.Run(new frmMenu());
         }
     }
 }
